ed7ea8fb10790bba0f1257c70a77be3afbbc1a71
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-04 14:24:23 -0400

    Implementing row decoding to remove extra hex char

--- sqlite_scraper.py
+++ sqlite_scraper.py
@@ -3,6 +3,8 @@ import sqlite3
 import time
 import string
 
+
+#Import extracted SQLite db
 filename = input("enter path to sqlite db: ")
 
 f = open(filename, "r")
@@ -10,8 +12,16 @@ f = open(filename, "r")
 conn = sqlite3.connect(filename)
 cur = conn.cursor()
 
-
+#Navigate to appropriate table and row
 res = cur.execute("SELECT name FROM sqlite_master WHERE name='object_data'")
 res.fetchall()
 print(res)
 
+#Set character validity for decoding
+valid_chars = string.printable
+
+#Decode and remove extraneous hex data, leaving only ascii characters
+for row in cur.execute("SELECT data FROM object_data"):
+    row_str = str(row)
+    row_decoded = ''.join(i for i in row_str if i in valid_chars)
+    print(row_decoded)
