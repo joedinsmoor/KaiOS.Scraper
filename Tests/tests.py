320951352260bdb97118d5d2421934edb1d531c7
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-04 22:38:45 -0400

    added actual test

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,5 +1,8 @@
-def func(x):
-    return x + 1
+import pytest 
 
-def test_answer():
-    assert func(3) == 5
+def f():
+    raise SystemExit(1)
+
+def test():
+    with pytest.raises(SystemExit):
+        f()
