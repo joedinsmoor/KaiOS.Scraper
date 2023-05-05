3ad5fe435d5746a21e62d895bf2455bf2af17e8d
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-04 22:29:25 -0400

    added ascii removal

--- sqlite_scraper.py
+++ sqlite_scraper.py
@@ -16,7 +16,6 @@ cur = conn.cursor()
 #Navigate to appropriate table and row
 res = cur.execute("SELECT name FROM sqlite_master WHERE name='object_data'")
 res.fetchall()
-print(res)
 
 #Set character validity for decoding
 valid_chars = string.printable
@@ -26,4 +25,5 @@ for row in cur.execute("SELECT data FROM object_data"):
     row_str = str(row)
     row_str_decoded = bytes(row_str, "utf-8").decode("unicode_escape")
     row_decoded = re.sub('r\\\\x[0-9a-fA-F]{2}', "", row_str_decoded)
-    print(row_decoded)
+    print_me = row_decoded.encode('ascii', 'ignore').decode('ascii')
+    print(print_me)
