26af2ddabb59256b0bd979b21be52aaa2c6d51e4
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-06 01:30:32 -0400

    Added image carving, nonfunctional as of 5/6

--- sqlite_scraper.py
+++ sqlite_scraper.py
@@ -3,6 +3,8 @@ import sqlite3
 import time
 import string
 import re
+from PIL import Image
+import io
 
 
 #Import extracted SQLite db
@@ -27,3 +29,17 @@ for row in cur.execute("SELECT data FROM object_data"):
     row_decoded = re.sub('r\\\\x[0-9a-fA-F]{2}', "", row_str_decoded)
     print_me = row_decoded.encode('ascii', 'ignore').decode('ascii')
     print(print_me)
+
+
+#Carve any images that exist in SQLite db - nonfunctional as of 5/6
+cur.execute("SELECT data FROM object_data")
+res = cur.fetchone()
+image_data = res[0]
+start_index = image_data.find(b'GIF89a')
+# end_index = image_data.find(b'\xff\xd9')
+image_binary = image_data[start_index:]
+
+image_stream = io.BytesIO(image_binary)
+image = Image.open(image_stream)
+
+image.show()
