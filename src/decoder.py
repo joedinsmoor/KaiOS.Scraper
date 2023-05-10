b525efe5bcba99904aca6af8ba05652d526c7d1b
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-09 23:37:59 -0400

    added phone number scraping

--- src/decoder.py
+++ src/decoder.py
@@ -1,7 +1,7 @@
 import re
 import sqlite3
 import string
-from logger import scraper_log
+from logger import scraper_log, phone_numbers
 
 
 #Decode and remove extraneous hex data, leaving only ascii characters
@@ -12,6 +12,8 @@ def decode(cur):
      row_str_decoded = bytes(row_str, "utf-8").decode("unicode_escape")
      row_decoded = re.sub('r\\\\x[0-9a-fA-F]{2}', "", row_str_decoded)
      print_me = row_decoded.encode('ascii', 'ignore').decode('ascii')
+     for i in re.findall(r'[\+\(]?[1-9][0-9 .\-(\)]{8,}[0-9]', print_me):
+            phone_numbers(i)
      print(print_me)
      scraper_log(print_me)
 
--- src/logger.py
+++ src/logger.py
@@ -3,3 +3,9 @@ def scraper_log(str):
     file.write("\n")
     file.write(str)
     file.close()
+
+def phone_numbers(str):
+    file = open("phone_numbers.log", "w")
+    file.write("\n")
+    file.write(str)
+    file.close()
