a31e52920d05f37e2ec6bd09e721ac3d12310549
joedinsmoor <jdinsmoor@dinsmoor.pw>
2023-05-22 09:22:19 -0400

    image handler rewritten

--- src/image_handler.py
+++ src/image_handler.py
@@ -13,13 +13,21 @@ import sqlite3
 def handle_photo(cur):
     #Carve any images that exist in SQLite db - nonfunctional as of 5/11
     cur.execute("SELECT data FROM object_data")
-    res = cur.fetchone()
-    image_data = res[0]
-    start_index = image_data.find(b'\xFF\xD8')
-    end_index = image_data.find(b'\xFF\xD9')
-    end = image_data.find(end_index, start_index) + len(end_index)
-    image_binary += image_data[start_index:end]
-    if image_binary != '':
-        with open(f'extracted-img.jpg', 'wb') as f:
-            f.write(image_binary)
+    res = cur.fetchall()
+    jpg_byte_start = b'\xff\xd8'
+    jpg_byte_end = b'\xff\xd9'
+    image_binary = bytearray()
+
+    start = res.find(jpg_byte_start)
+    if start == -1:
+        print("Could not find .jpg file within Sqlite db")
+        return
+    end = res.find(jpg_byte_end, start) + len(jpg_byte_end)
+
+    image_binary += res[start:end]
+    print(f"Size: {end - start} bytes")
+
+    with open(f'extracted_image.jpg', 'wb') as f:
+        f.write(image_binary)
+    
     
